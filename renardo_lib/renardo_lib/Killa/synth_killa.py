cdd28673ecc685d7c25cf825c3682b69a6c86e2f
iShapeNoise <contact@jensmeisner.net>
2021-02-10 23:23:42 +0100

    Add meta-data block and filled in known information, add new synths, rename and fix old synths
    (cherry picked from commit badc894065c1d3860bb6080cf077526ce9a38a57)

--- renardo_lib/renardo_lib/Killa/synth_killa.py
+++ renardo_lib/renardo_lib/Killa/synth_killa.py
@@ -2,23 +2,265 @@
 
 from ..SCLang.SynthDef import FileSynthDef
 
+
+acidbass = FileSynthDef("acidbass")
+acidbass.add()
+
+bbass = FileSynthDef("bbass")
+bbass.add()
+
+bchaos = FileSynthDef("bchaos")
+bchaos.add()
+
+benoit = FileSynthDef("benoit")
+benoit.add()
+
+blips = FileSynthDef("blips")
+blips.add()
+
+borgan = FileSynthDef("borgan")
+borgan.add()
+
+bounce = FileSynthDef("bounce")
+bounce.add()
+
+bphase = FileSynthDef("bphase")
+bphase.add()
+
+chimebell = FileSynthDef("chimebell")
+chimebell.add()
+
+chipsy = FileSynthDef("chipsy")
+chipsy.add()
+
+click = FileSynthDef("click")
+click.add()
+
+cluster = FileSynthDef("cluster")
+cluster.add()
+
+combs = FileSynthDef("combs")
+combs.add()
+
+cs80lead = FileSynthDef("cs80lead")
+cs80lead.add()
+
+dafbass = FileSynthDef("dafbass")
+dafbass.add()
+
+dblbass = FileSynthDef("dblbass")
+dblbass.add()
+
+donorgan = FileSynthDef("donorgan")
+donorgan.add()
+
+drone = FileSynthDef("drone")
+drone.add()
+
+epiano = FileSynthDef("epiano")
+epiano.add()
+
+faim = FileSynthDef("faim")
+faim.add()
+
+filthysaw = FileSynthDef("filthysaw")
+filthysaw.add()
+
+flute = FileSynthDef("flute")
+flute.add()
+
+fmbass = FileSynthDef("fmbass")
+fmbass.add()
+
+fmrhodes = FileSynthDef("fmrhodes")
+fmrhodes.add()
+
+garfield = FileSynthDef("garfield")
+garfield.add()
+
+harp = FileSynthDef("harp")
+harp.add()
+
+hoover = FileSynthDef("hoover")
+hoover.add()
+
+kalimba = FileSynthDef("kalimba")
+kalimba.add()
+
+laserbeam = FileSynthDef("laserbeam")
+laserbeam.add()
+
+lazer = FileSynthDef("lazer")
+lazer.add()
+
+linesaw = FileSynthDef("linesaw")
+linesaw.add()
+
+longsaw = FileSynthDef("longsaw")
+longsaw.add()
+
+mhpad = FileSynthDef("mhpad")
+mhpad.add()
+
+mhping = FileSynthDef("mhping")
+mhping.add()
+
+moogbass = FileSynthDef("moogbass")
+moogbass.add()
+
 moogpluck = FileSynthDef("moogpluck")
 moogpluck.add()
+
+moogpluck2 = FileSynthDef("moogpluck2")
+moogpluck2.add()
+
+noisynth = FileSynthDef("noisynth")
+noisynth.add()
+
+noquarter = FileSynthDef("noquarter")
+noquarter.add()
 
 organ = FileSynthDef("organ")
 organ.add()
 
+organ2 = FileSynthDef("organ2")
+organ2.add()
+
+pbass = FileSynthDef("pbass")
+pbass.add()
+
+phazer = FileSynthDef("phazer")
+phazer.add()
+
 pianovel = FileSynthDef("pianovel")
 pianovel.add()
+
+pmcrotal = FileSynthDef("pmcrotal")
+pmcrotal.add()
+
+prayerbell = FileSynthDef("prayerbell")
+prayerbell.add()
+
+ppad = FileSynthDef("ppad")
+ppad.add()
 
 rhodes = FileSynthDef("rhodes")
 rhodes.add()
 
+rhpiano = FileSynthDef("rhpiano")
+rhpiano.add()
+
+risseto = FileSynthDef("risseto")
+risseto.add()
+
+rissetobell = FileSynthDef("rissetobell")
+rissetobell.add()
+
+rlead = FileSynthDef("rlead")
+rlead.add()
+
+rsaw = FileSynthDef("rsaw")
+rsaw.add()
+
+rsin = FileSynthDef("rsin")
+rsin.add()
+
+scatter = FileSynthDef("scatter")
+scatter.add()
+
+shore = FileSynthDef("shore")
+shore.add()
+
+sillyvoice = FileSynthDef("sillyvoice")
+sillyvoice.add()
+
 sine = FileSynthDef("sine")
 sine.add()
+
+siren = FileSynthDef("siren")
+siren.add()
+
+spacesaw = FileSynthDef("spacesaw")
+spacesaw.add()
+
+spick = FileSynthDef("spick")
+spick.add()
+
+strings = FileSynthDef("strings")
+strings.add()
+
+sosbell = FileSynthDef("sosbell")
+sosbell.add()
+
+soundin = FileSynthDef("soundin")
+soundin.add()
+
+sputter = FileSynthDef("sputter")
+sputter.add()
+
+square = FileSynthDef("square")
+square.add()
+
+steeldrum = FileSynthDef("steeldrum")
+steeldrum.add()
+
+subbass = FileSynthDef("subbass")
+subbass.add()
+
+subbass2 = FileSynthDef("subbass2")
+subbass2.add()
 
 supersaw = FileSynthDef("supersaw")
 supersaw.add()
 
 tb303 = FileSynthDef("tb303")
 tb303.add()
+
+tremsynth = FileSynthDef("tremsynth")
+tremsynth.add()
+
+tribell = FileSynthDef("tribell")
+tribell.add()
+
+triwave = FileSynthDef("triwave")
+triwave.add()
+
+tubularbell = FileSynthDef("tubularbell")
+tubularbell.add()
+
+tworgan = FileSynthDef("tworgan")
+tworgan.add()
+
+tworgan2 = FileSynthDef("tworgan2")
+tworgan2.add()
+
+tworgan3 = FileSynthDef("tworgan3")
+tworgan3.add()
+
+tworgan4= FileSynthDef("tworgan4")
+tworgan4.add()
+
+varcell = FileSynthDef("varcell")
+varcell.add()
+
+vibass = FileSynthDef("vibass")
+vibass.add()
+
+vinsine = FileSynthDef("vinsine")
+visine.add()
+
+windmaker = FileSynthDef("windmaker")
+windmaker.add()
+
+wobble = FileSynthDef("wobble")
+wobble.add()
+
+wsaw = FileSynthDef("wsaw")
+wsaw.add()
+
+wsawbass = FileSynthDef("wsawbass")
+wsawbass.add()
+
+xylophone = FileSynthDef("xylophone")
+xylophone.add()
+
