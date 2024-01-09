8a651ee33d8a43b3d1d97a9db2a918fad81b0c49
Elie Gavoty <eliegavoty@free.fr>
2024-01-09 01:06:43 +0100

    bump versions for release 0.9.1

--- FoxDotEditor/setup.py
+++ FoxDotEditor/setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as f:
 
 setup(
     name='FoxDotEditor',
-    version="0.9.1.dev4",
+    version="0.9.1",
     description='Original FoxDot editor extracted from FoxDot Project - Live coding music with SuperCollider',
     author='Elie Gavoty',
     author_email='eliegavoty@free.fr',
@@ -20,7 +20,7 @@ setup(
     # data_files=[('', 'LICENSE')],
     package_data = {'FoxDotEditor': ['README.md','img/*','tmp/*'],},
     install_requires=[
-        'renardo-lib>=0.9.1.dev2',
+        'renardo-lib>=0.9.1',
         'psutil',
     ],
 )
--- renardo_lib/setup.py
+++ renardo_lib/setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as f:
 
 setup(
     name='renardo_lib',
-    version="0.9.1.dev3",
+    version="0.9.1",
     description='Python livecoding environment - New fork of FoxDot',
     author='Elie Gavoty',
     author_email='eliegavoty@free.fr',
@@ -34,6 +34,6 @@ setup(
     # data_files=[('', 'LICENSE')],
     package_data = {'renardo_lib': ['README.md','demo/**', 'osc/**'],},
     install_requires=[
-        'renardo_gatherer>=0.1.0.dev1'
+        'renardo_gatherer>=0.1.0'
     ],
 )
