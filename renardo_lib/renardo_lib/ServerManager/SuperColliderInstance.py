8e2d78c499412479224fb09920e71c96a1cca7d8
= <=>
2024-01-16 13:36:32 +0100

    lib: fix auto launch sclang on windows

--- renardo_lib/renardo_lib/ServerManager/SuperColliderInstance.py
+++ renardo_lib/renardo_lib/ServerManager/SuperColliderInstance.py
@@ -1,5 +1,8 @@
+import os
 import subprocess
+from sys import platform
 import time
+import pathlib
 
 import psutil
 
@@ -7,12 +10,27 @@ from renardo_lib.ServerManager.SCFilesHandling import SC_USER_CONFIG_DIR
 
 
 class RenardoSupercolliderInstance:
+
     def __init__(self):
         self.popen = None
+
+        #process_env = os.environ.copy()
+        if platform == "win32":
+            sc_dir = next(pathlib.Path("C:\\Program Files").glob("SuperCollider*")) # to match SuperCollider-3.version like folders
+            sclang_path = sc_dir / "sclang.exe"
+            #process_env["PATH"] = process_env + f"{sc_dir};"
+            sclang_exec = [str(sclang_path), str(SC_USER_CONFIG_DIR / 'start_renardo.scd')]
+        else:
+            sclang_exec = ["sclang",  SC_USER_CONFIG_DIR / 'start_renardo.scd']
+
         if not self.is_sclang_running():
+            #self.popen = subprocess.Popen(["cmd", "/c", "dir", "c:\\Users"], stdout=subprocess.PIPE)
+            print("Auto Launching Renardo SC module with SCLang...")
             self.popen = subprocess.Popen(
-                args=['sclang', SC_USER_CONFIG_DIR / 'start_renardo.scd'],
-                shell=False,
+                args=sclang_exec,
+                shell=True,
+                stdout=subprocess.PIPE,
+                #env=process_env,
             )
             time.sleep(1)  # wait a bit for the server to be started
         # TODO : find a way to consistently stop sclang and scsynth when renardo stops/dies
@@ -20,9 +38,10 @@ class RenardoSupercolliderInstance:
         # TODO : Use name renardo for scsynth audio server (for example with JACK Driver)
 
     def __del__(self):
-        self.popen.kill() # TODO: fix that the destructor is not called
+        pass
+        # self.popen.kill() # TODO: fix that the destructor is not called
         # need to clarify the launch and close process of foxdot/renardo !
-        self.popen.wait()
+        # self.popen.wait()
 
     def is_sclang_running(self):
         running = False
