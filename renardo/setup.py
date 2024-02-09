49ce23cebeb86c98c9c30ce29f1a891c1d837e9e
Elie Gavoty <eliegavoty@free.fr>
2024-02-09 04:18:39 +0100

    all: bump minor version for bugfix

--- renardo/setup.py
+++ renardo/setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as f:
 
 setup(
     name='renardo',
-    version="0.9.4",
+    version="0.9.5",
     description='Launcher/config editor for Renardo livecoding environment',
     author='Elie Gavoty',
     author_email='eliegavoty@free.fr',
@@ -22,8 +22,8 @@ setup(
     # data_files=[('', 'LICENSE')],
     package_data = {'renardo': ['RenardoTUI.tcss'],},
     install_requires=[
-        'renardo-lib==0.9.4',
-        'FoxDotEditor==0.9.4',
+        'renardo-lib==0.9.5',
+        'FoxDotEditor==0.9.5',
         'renardo_gatherer==0.1.3',
         'psutil',
         'textual',
