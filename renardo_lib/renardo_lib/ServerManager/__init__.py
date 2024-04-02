56544f54c5d8f08d384a7abb8491c3a22026bbc0
Elie Gavoty <eliegavoty@free.fr>
2024-04-02 15:56:47 +0200

    lib: fix reaper integration bugs

--- renardo_lib/renardo_lib/ServerManager/__init__.py
+++ renardo_lib/renardo_lib/ServerManager/__init__.py
@@ -505,8 +505,7 @@ class SCLangServerManager(ServerManager):
 
         # Create a specific message for midi
 
-        if synthdef == "MidiOut":  # this should be in a dict of synthdef to functions maybe? we need a "nudge to sync"
-
+        if synthdef in ["MidiOut", "ReaperInstrument"]:  # this should be in a dict of synthdef to functions maybe? we need a "nudge to sync"
             return self.get_midi_message(synthdef, packet, timestamp)
 
         # Create a bundle
