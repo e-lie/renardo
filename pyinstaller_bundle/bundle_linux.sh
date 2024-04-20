620502b1f14bffebc6e52537668c6e978b4a7673
Elie Gavoty <eliegavoty@free.fr>
2024-04-20 19:52:07 +0200

    all: bump versions for v0.9.10

--- pyinstaller_bundle/bundle_linux.sh
+++ pyinstaller_bundle/bundle_linux.sh
@@ -1,4 +1,4 @@
-export RENARDO_VERSION="0.9.9"
+export RENARDO_VERSION="0.9.10"
 
 echo '######## creating venv'
 rm -Rf /tmp/venv_pyinstaller
--- renardo/setup.py
+++ renardo/setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as f:
 
 setup(
     name='renardo',
-    version="0.9.10.dev0",
+    version="0.9.10",
     description='Launcher/config editor for Renardo livecoding environment',
     author='Elie Gavoty',
     author_email='eliegavoty@free.fr',
@@ -23,8 +23,8 @@ setup(
     # data_files=[('', 'LICENSE')],
     package_data = {'renardo': ['RenardoTUI.tcss'],},
     install_requires=[
-        'renardo-lib==0.9.10.dev0',
-        'FoxDotEditor==0.9.10.dev0',
+        'renardo-lib==0.9.10',
+        'FoxDotEditor==0.9.10',
         'renardo_gatherer==0.1.3',
         'psutil',
         'textual',
