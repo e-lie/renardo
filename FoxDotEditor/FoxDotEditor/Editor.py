fd16e230d73bde9217be1b0b3b664f5d96a691e7
Elie Gavoty <eliegavoty@free.fr>
2024-04-21 11:32:39 +0200

    all: bump versions to 0.9.11.dev1

--- FoxDotEditor/FoxDotEditor/Editor.py
+++ FoxDotEditor/FoxDotEditor/Editor.py
@@ -60,7 +60,7 @@ class workspace:
         CodeClass.namespace['GUI'] = self
         CodeClass.namespace['Player'].widget = self
 
-        self.version = this_version = '0.9.3' 
+        self.version = this_version = '0.9.11.dev1' 
 
         pypi_version = get_pypi_version()
 
