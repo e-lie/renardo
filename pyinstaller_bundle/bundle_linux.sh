6d7ff214d5449bcef8080aa9b320eaf67e96fccb
Elie Gavoty <eliegavoty@free.fr>
2024-04-21 22:04:48 +0200

    lib: fix setup.py add TempoClock module + bum dev version

--- pyinstaller_bundle/bundle_linux.sh
+++ pyinstaller_bundle/bundle_linux.sh
@@ -1,4 +1,4 @@
-export RENARDO_VERSION="1.0.0.dev2"
+export RENARDO_VERSION="1.0.0.dev3"
 
 echo '######## creating venv'
 rm -Rf /tmp/venv_pyinstaller
--- renardo_lib/setup.py
+++ renardo_lib/setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as f:
 
 setup(
     name='renardo_lib',
-    version="1.0.0.dev2",
+    version="1.0.0.dev3",
     description='Python livecoding environment - New fork of FoxDot',
     author='Elie Gavoty',
     author_email='eliegavoty@free.fr',
@@ -28,6 +28,7 @@ setup(
         'renardo_lib.Settings',
         'renardo_lib.Utils',
         'renardo_lib.ServerManager',
+        'renardo_lib.TempoClock',
     ],
     long_description=long_description,
     long_description_content_type="text/markdown",
