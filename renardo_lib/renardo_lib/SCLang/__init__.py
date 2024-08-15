e585176cfa9e795c6fd61590a0da06e2c979e6b3
Elie Gavoty <mail@eliegavoty.fr>
2024-08-15 13:47:45 +0200

    lib: refacto rename SynthDef to DefaultSynthDef and Buffers to BufferManagement

--- renardo_lib/renardo_lib/SCLang/__init__.py
+++ renardo_lib/renardo_lib/SCLang/__init__.py
@@ -1,7 +1,6 @@
 from renardo_lib.SCLang.SCLangExperimentalBindings import *
-from renardo_lib.SCLang.SynthDef import SynthDefs, SynthDef
+from renardo_lib.SCLang.SynthDef import SynthDefs, DefaultSynthDef
 from renardo_lib.SCLang.SynthDefProxy import SynthDefProxy
 from renardo_lib.SCLang.SampleSynthDefs import SampleSynthDef
-# from renardo_lib.SCLang import Env
 
 
