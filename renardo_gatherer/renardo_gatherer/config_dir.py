945344c7624cbdc85eee4294fbd0af0a821222d6
Elie Gavoty <mail@eliegavoty.fr>
2024-08-15 19:45:01 +0200

    lib: refacto move sclang code

--- renardo_gatherer/renardo_gatherer/config_dir.py
+++ renardo_gatherer/renardo_gatherer/config_dir.py
@@ -2,7 +2,7 @@ import os
 import pathlib
 from sys import platform
 
-SAMPLES_DIR_PATH = None
+USER_CONFIG_DIR_PATH = None
 
 # default config path
 # on windows AppData/Roaming/renardo
@@ -10,12 +10,15 @@ SAMPLES_DIR_PATH = None
 # on MacOS /Users/<username>/Library/Application Support/renardo
 if platform == "linux" or platform == "linux2" :
     home_path = pathlib.Path.home()
-    SAMPLES_DIR_PATH = home_path / '.config' / 'renardo' / 'samples'
+    USER_CONFIG_DIR_PATH = home_path / '.config' / 'renardo'
 elif platform == "darwin":
     home_path = pathlib.Path.home()
-    SAMPLES_DIR_PATH = home_path / 'Library' / 'Application Support' / 'renardo' / 'samples'
+    USER_CONFIG_DIR_PATH = home_path / 'Library' / 'Application Support' / 'renardo'
 elif platform == "win32":
     appdata_roaming_path = pathlib.Path(os.getenv('APPDATA'))
-    SAMPLES_DIR_PATH = appdata_roaming_path / 'renardo' / 'samples'
+    USER_CONFIG_DIR_PATH = appdata_roaming_path / 'renardo'
 
+def get_samples_dir_path():
+    return USER_CONFIG_DIR_PATH / 'samples'
 
+SAMPLES_DIR_PATH = get_samples_dir_path()
