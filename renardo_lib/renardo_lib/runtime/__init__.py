29bf815ad756a8f19fb33da4d4e35f8aabfcc810
Elie Gavoty <mail@eliegavoty.fr>
2024-08-16 19:06:35 +0200

    all: fix renardo_lib packaging and bump dev versions

--- renardo_lib/renardo_lib/runtime/__init__.py
+++ renardo_lib/renardo_lib/runtime/__init__.py
@@ -211,7 +211,7 @@ def Go():
 # Util class
 class _util:
     def __repr__(self):
-        return "Renardo ver. 0.9.13.dev2"
+        return "Renardo ver. 0.9.13.dev3"
     def reload(self):
         Server.init_connection()
         SynthDefs.reload()
