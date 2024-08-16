7cde8d19a818b8898d4a57947ec21b1cba5759a9
Elie Gavoty <mail@eliegavoty.fr>
2024-08-16 20:59:50 +0200

    lib: reenable all the synthdefs

--- renardo_lib/renardo_lib/runtime/synthdefs_initialisation/__init__.py
+++ renardo_lib/renardo_lib/runtime/synthdefs_initialisation/__init__.py
@@ -1,7 +1,7 @@
 from renardo_lib.SynthDefManagement.SimpleSynthDefs import LiveSynthDef
 from .python_defined_effect_synthdefs import *
-#from .special_synthdefs import *
-#from .python_defined_synthdefs import *
+from .special_synthdefs import *
+from .python_defined_synthdefs import *
 from .sclang_file_synthdefs import *
 
 
