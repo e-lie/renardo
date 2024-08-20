d3ae5f97fa57365ec7694db0bf3945f497c7b7d1
noisk8 <juansk8jaramillo@gmail.com>
2024-08-19 19:23:43 -0500

    start translate to spanish

---
+++ renardo_lib/renardo_lib/demo_esp/00_introduccion.py
@@ -0,0 +1,21 @@
+# Tutorial 0: Introducción
+
+################
+# Ejecutanco código
+
+# Para ejecutar código en FoxDot, asegúrese de que el cursor de texto se encuentra en el «bloque» de código
+# (secciones de texto no separadas por líneas en blanco) y pulse Ctrl+Enter
+
+#  Para ejecutar una sola línea, incluso en un bloque, pulse Alt+Retorno
+
+# Pruébalo ahora, mueve el cursor a la línea de código de abajo y pulsa Ctrl+Return
+
+print("Hola Mundo")
+
+##############
+# Ayuda
+
+# Si alguna vez estás atascado, o quieres saber más sobre una función o clase
+# simplemente escribe help seguido del nombre de ese objeto Python entre paréntesis:
+
+help(object)
