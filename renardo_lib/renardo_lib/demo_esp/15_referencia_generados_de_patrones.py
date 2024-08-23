d8c014aa703b958ef27d3b3c2df1a2847bab4d32
Noisk8 <juansk8jaramillo@gmail.com>
2024-08-23 00:12:33 -0500

    add last  tutorial

---
+++ renardo_lib/renardo_lib/demo_esp/15_referencia_generados_de_patrones.py
@@ -0,0 +1,17 @@
+# Tutorial 15  Referencia generadpres de patrones
+
+# Hay otras clases de Patrones en FoxDot que te ayudan a generar matrices de números pero también se comportan
+# de la misma manera que el Patrón base. Para ver que Patrones existen y probar a usarlos, ejecute
+print(classes(Patterns.Sequences))
+
+####################
+# PEuclid
+# PEuclid(n, k)
+# Devuelve el ritmo euclidiano que reparte 'n' pulsos en 'k' pasos de la forma más uniforme posible.
+
+# 3 pulsos sobre 8 pasos
+print(PEuclid(3, 8))
+
+####################
+# PDur
+# PDur(n, k, start=0, dur=0.25)
