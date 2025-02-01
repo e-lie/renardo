7defdcb7d820e003cced9a3dbf8317be89e32de5
Elie Gavoty <mail@eliegavoty.fr>
2025-02-01 17:50:22 +0100

    gatherer: refacto using hatchling

---
+++ renardo_gatherer/tests/__init__.py
@@ -0,0 +1,10 @@
+# SPDX-FileCopyrightText: 2024-present Elie Gavoty <mail@eliegavoty.fr>
+#
+# SPDX-License-Identifier: MIT
+
+
+# from renardo_reaper.reaper_integration import get_reaper_track_names
+
+# def test_get_reaper_track_names():
+#     track_names = get_reaper_track_names()
+#     assert isinstance(track_names, list), "Track names should be a list"
