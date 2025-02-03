145067499b0873c78a87b7e673e695ffcdd3923f
Elie Gavoty <mail@eliegavoty.fr>
2025-02-03 18:31:09 +0100

    add new python package to move SC backend out

---
+++ renardo_sc_backend/src/renardo_sc_backend/__init__.py
@@ -0,0 +1,8 @@
+
+# Expose all
+# from renardo_gatherer.sample_management import sample_file, sample_category, sample_pack, sample_pack_dict, default_samples
+
+# Expose effectively used only
+from renardo_sc_backend.buffer_manager import BufferManager
+
+buffer_manager = BufferManager()
