9bd90e847ba050fcdadb5b9303855c4f99fb7f48
Elie Gavoty <mail@eliegavoty.fr>
2025-02-03 18:31:09 +0100

    add new python package to move SC backend out
    (cherry picked from commit 03ae5e214fe42bc20d15816be148ea8033eafbce)

---
+++ renardo_sc_backend/tests/__init__.py
@@ -0,0 +1,9 @@
+# SPDX-FileCopyrightText: 2024-present Elie Gavoty <mail@eliegavoty.fr>
+#
+# SPDX-License-Identifier: MIT
+
+
+
+# def test_get_reaper_track_names():
+#     track_names = get_reaper_track_names()
+#     assert isinstance(track_names, list), "Track names should be a list"
