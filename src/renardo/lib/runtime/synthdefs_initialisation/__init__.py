87536ee74682b352ae3151d2a43d83b40aab6fab
Elie Gavoty <mail@eliegavoty.fr>
2025-04-04 21:23:24 +0200

    sc_backend: finish migrate instruments loading to sccode_library
    (cherry picked from commit b392f50b936ecf4ccf6a7244febfcf8c83c1603b)

--- src/renardo/lib/runtime/synthdefs_initialisation/__init__.py
+++ src/renardo/lib/runtime/synthdefs_initialisation/__init__.py
@@ -1,9 +1,6 @@
 from renardo.sc_backend import LiveSynthDef, FileSynthDef
 from .python_defined_effect_synthdefs import effect_manager, PygenEffect, In, Out
-
-# from .python_defined_effect_synthdefs import (
-#     PygenEffect, In, Out, Server, effect_manager, fx
-# )
+from pathlib import Path
 
 from renardo.gatherer import sccode_library
 from renardo.settings_manager import settings
@@ -14,13 +11,16 @@ for sccode_bank in sccode_library:
         pass
         for instrument_category in sccode_bank.instruments:
             for sccode_file in instrument_category:
-                globals()[sccode_file.name] = FileSynthDef(sccode_file.name)
+                globals()[sccode_file.name] = FileSynthDef(name=sccode_file.name, sccode_path=sccode_file.path)
+
+# create the special sccode dir in the user dir if not exist
+Path(settings.get("sc_backend.PLAY_SCCODE_DIR")).mkdir(parents=True, exist_ok=True)
 
 # The 1 or 2 channels buffer strategy of play (which is not working) implies a specific setup with two player
-play = FileSynthDef('play1')
+play = FileSynthDef('play1', Path(settings.get("sc_backend.PLAY_SCCODE_DIR")) / 'play1.scd')
 play.defaults["dur"]=.5
 play.add()
-play = FileSynthDef('play2')
+play = FileSynthDef('play2', Path(settings.get("sc_backend.PLAY_SCCODE_DIR")) / 'play2.scd')
 play.defaults["dur"]=.5
 play.add()
 
