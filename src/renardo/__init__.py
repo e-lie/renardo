b08a3d83d43a5a1c93f6ccfe0b9dfe8d78465254
Elie Gavoty <mail@eliegavoty.fr>
2025-04-04 19:14:42 +0200

    renardo: fix __init__ of the module to be able to just import the lib

--- src/renardo/__init__.py
+++ src/renardo/__init__.py
@@ -6,11 +6,6 @@ from renardo.RenardoApp import RenardoApp
 def main() -> None:
    RenardoApp()
 
-RenardoApp()
-
-# from .RenardoApp import RenardoApp
-
 # main is to call the module with python -m but we want to make a pypi package application with entry_point
 # More here : https://setuptools.pypa.io/en/latest/userguide/entry_point.html # we don't use setuptools anymore but hatch
 
-# RenardoApp()
