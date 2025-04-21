0350f45f7aa1a9d2ddc1fc8eff43c3f9d73b56ce
Elie Gavoty <mail@eliegavoty.fr>
2025-04-21 17:47:18 +0200

    settings: for paths use dynamic resolution rather than settings using unified method settings.get_path
    (cherry picked from commit c20aabb2ddb79a544f492a3482e08b64cbddfc60)

--- src/renardo/foxdot_editor/Treeview.py
+++ src/renardo/foxdot_editor/Treeview.py
@@ -41,8 +41,8 @@ class TreeView:
         # This dictionary maps the treeview items IDs with the
         # path of the file or folder.
         self.fsobjects: dict[str, Path] = {}
-        self.file_image = PhotoImage(file=settings.get("foxdot_editor.FOXDOT_EDITOR_ROOT")+"/img/file.png")
-        self.folder_image = PhotoImage(file=settings.get("foxdot_editor.FOXDOT_EDITOR_ROOT")+"/img/folder.png")
+        self.file_image = PhotoImage(file=str(settings.get_path("FOXDOT_EDITOR_ROOT"))+"/img/file.png")
+        self.folder_image = PhotoImage(file=str(settings.get_path("FOXDOT_EDITOR_ROOT"))+"/img/folder.png")
         # Load the user directory.
         self.home_path = str(Path.home())
         self.load_tree(Path(os.path.expanduser(self.home_path)))
