2ec3abad748bc768ec69617295021eb08eb55bba
Elie Gavoty <mail@eliegavoty.fr>
2025-05-06 18:52:46 +0200

    add new example webclient: flask + svelte + websocket connection

--- src/renardo/__init__.py
+++ src/renardo/__init__.py
@@ -1,8 +1,11 @@
 from renardo.settings_manager import settings
 from renardo.RenardoApp import RenardoApp
 
+from renardo.webserver import run
+
 def main() -> None:
-   RenardoApp()
+   #RenardoApp()
+   run()
 
 # main is to call the module with python -m but we want to make a pypi package application with entry_point
 # More here : https://setuptools.pypa.io/en/latest/userguide/entry_point.html # we don't use setuptools anymore but hatch
--- src/renardo/__main__.py
+++ src/renardo/__main__.py
@@ -3,4 +3,5 @@ from .RenardoApp import RenardoApp
 # main is to call the module with python -m but we want to make a pypi package application with entry_point
 # More here : https://setuptools.pypa.io/en/latest/userguide/entry_point.html # we don't use setuptools anymore but hatch
 
-RenardoApp()
+#RenardoApp()
+
