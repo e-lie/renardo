06fbea7884146c4a8a1af34a50bb0c9be08ddbeb
Elie Gavoty <mail@eliegavoty.fr>
2025-05-06 21:07:52 +0200

    livecoding editor with CodeMirror WIP

--- webclient/vite.config.js
+++ webclient/vite.config.js
@@ -4,6 +4,9 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  optimizeDeps: {
+    exclude: ["svelte-codemirror-editor", "codemirror", "@codemirror/language-javascript" /* ... */],
+  },
   server: {
     proxy: {
       '/api': {
