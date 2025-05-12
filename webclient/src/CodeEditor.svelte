5e10ba6a0e6d14b247afbeee90d4b95cdc11eed7
Elie Gavoty <mail@eliegavoty.fr>
2025-05-12 16:52:23 +0200

    improve editor configuration section

--- webclient/src/CodeEditor.svelte
+++ webclient/src/CodeEditor.svelte
@@ -158,15 +158,19 @@ d2 >> blip([_,_,4,_], dur=.5)
             'Cmd-.': stopMusic              // For Mac
           });
 
-          // Initialize theme in the theme selector component
-          // This will also apply any previously saved theme preference
-          if (editor) {
-            // This component has a method to initialize with the editor instance
-            editor.on('refresh', () => {
-              // Let the theme selector know about the editor
-              // This will apply any saved theme preferences
-            });
-          }
+          // For the editor to be properly initialized with the theme,
+          // we need to make sure we're using the right settings from local storage
+          const savedTheme = localStorage.getItem('editor-theme') || 'monokai';
+          const showLineNumbers = localStorage.getItem('editor-show-line-numbers') !== 'false';
+
+          // Apply settings
+          editor.setOption('theme', savedTheme);
+          editor.setOption('lineNumbers', showLineNumbers);
+
+          // Refresh the editor to apply all settings
+          setTimeout(() => {
+            editor.refresh();
+          }, 100);
           
           // Log successful initialization
           console.log("CodeMirror editor initialized with Python syntax highlighting");
@@ -433,44 +437,46 @@ d2 >> blip([_,_,4,_], dur=.5)
         <span class="badge badge-sm">Run Code button: Run all code</span>
       </div>
 
-      <div class="flex flex-wrap gap-2">
-        <button
-          class="btn btn-sm btn-success"
-          on:click={() => sendCodeToExecute(editor.getValue(), 'all')}
-          title="Run all code in editor"
-        >
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-1" viewBox="0 0 20 20" fill="currentColor">
-            <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z" clip-rule="evenodd" />
-          </svg>
-          Run Code
-        </button>
+      <div class="flex flex-wrap justify-between items-center gap-2">
+        <div class="flex flex-wrap gap-2">
+          <button
+            class="btn btn-sm btn-success"
+            on:click={() => sendCodeToExecute(editor.getValue(), 'all')}
+            title="Run all code in editor"
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-1" viewBox="0 0 20 20" fill="currentColor">
+              <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z" clip-rule="evenodd" />
+            </svg>
+            Run Code
+          </button>
 
-        <button
-          class="btn btn-sm btn-warning"
-          on:click={stopMusic}
-          title="Stop all music playback"
-        >
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-1" viewBox="0 0 20 20" fill="currentColor">
-            <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8 7a1 1 0 00-1 1v4a1 1 0 001 1h4a1 1 0 001-1V8a1 1 0 00-1-1H8z" clip-rule="evenodd" />
-          </svg>
-          Stop Music
-        </button>
+          <button
+            class="btn btn-sm btn-warning"
+            on:click={stopMusic}
+            title="Stop all music playback"
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-1" viewBox="0 0 20 20" fill="currentColor">
+              <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8 7a1 1 0 00-1 1v4a1 1 0 001 1h4a1 1 0 001-1V8a1 1 0 00-1-1H8z" clip-rule="evenodd" />
+            </svg>
+            Stop Music
+          </button>
 
-        <button
-          class="btn btn-sm btn-error"
-          on:click={clearConsole}
-          title="Clear console output"
-        >
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-1" viewBox="0 0 20 20" fill="currentColor">
-            <path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd" />
-          </svg>
-          Clear Console
-        </button>
+          <button
+            class="btn btn-sm btn-error"
+            on:click={clearConsole}
+            title="Clear console output"
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-1" viewBox="0 0 20 20" fill="currentColor">
+              <path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd" />
+            </svg>
+            Clear Console
+          </button>
+        </div>
+
+        <!-- Editor Theme Selector -->
+        <CodeMirrorThemeSelector bind:editor={editor} />
       </div>
     </div>
-
-    <!-- Editor Theme Selector -->
-    <CodeMirrorThemeSelector bind:editor={editor} />
   </div>
 
   <!-- Main workspace -->
