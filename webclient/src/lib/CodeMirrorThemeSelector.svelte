90a85041696536139f4487152667dbe32634d54e
Elie Gavoty <mail@eliegavoty.fr>
2025-05-12 16:47:09 +0200

    fix solarized

--- webclient/src/lib/CodeMirrorThemeSelector.svelte
+++ webclient/src/lib/CodeMirrorThemeSelector.svelte
@@ -7,6 +7,8 @@
     { name: "Dracula", value: "dracula" },
     { name: "Material", value: "material" },
     { name: "Nord", value: "nord" },
+    { name: "Solarized Dark", value: "solarized-dark" },
+    { name: "Solarized Light", value: "solarized-light" },
     { name: "Darcula", value: "darcula" },
     { name: "Eclipse", value: "eclipse" }
   ];
