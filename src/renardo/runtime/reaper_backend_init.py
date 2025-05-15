66e1842bc69eaa3161f311e370d35c5a521b4809
Elie Gavoty <mail@eliegavoty.fr>
2025-05-15 18:59:30 +0200

    chore: conditional init of reaper backend in renardo.runtime

---
+++ src/renardo/runtime/reaper_backend_init.py
@@ -0,0 +1,10 @@
+from renardo.reaper_backend.ReaperIntegration import init_reapy_project, ReaperInstrumentFactory, ReaTask
+
+old_style_presets = {}
+
+reaproject = init_reapy_project()
+reainstru_factory = ReaperInstrumentFactory(old_style_presets, reaproject)
+#reaper_instruments = reainstru_factory.create_all_facades_from_reaproject_tracks()
+
+add_chains = reainstru_factory.add_chains
+instanciate = reainstru_factory.instanciate
