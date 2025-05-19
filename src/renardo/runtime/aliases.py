bb109a32dc8cc6ec182c78f1073eb64426991360
Elie Gavoty <mail@eliegavoty.fr>
2025-05-20 00:14:40 +0200

    feat: migrate to new Class in runtime ec

--- src/renardo/runtime/aliases.py
+++ src/renardo/runtime/aliases.py
@@ -3,13 +3,13 @@ from .reaper_backend_init import reainstru_factory
 
 
 # Reaper integration
-add_multiple_fxchains = reainstru_factory.add_multiple_fxchains
-create_instrument_facade = reainstru_factory.create_instrument_facade
+# add_multiple_fxchains = reainstru_factory.add_multiple_fxchains
+# create_instrument_facade = reainstru_factory.create_instrument_facade
 
-def instanciate(track_name, chain_name, scan_all_params=True, is_chain=True):
-    try:
-        facade_obj = create_instrument_facade(name=chain_name, plugin_name=chain_name, track_name=track_name, scan_all_params=scan_all_params, is_chain=is_chain)
-        return facade_obj
-    except Exception as e:
-        print(f"Error adding chain {chain_name}: {e}")
-        return None
+# def instanciate(track_name, chain_name, scan_all_params=True, is_chain=True):
+#     try:
+#         facade_obj = create_instrument_facade(name=chain_name, plugin_name=chain_name, track_name=track_name, scan_all_params=scan_all_params, is_chain=is_chain)
+#         return facade_obj
+#     except Exception as e:
+#         print(f"Error adding chain {chain_name}: {e}")
+#         return None
--- src/renardo/runtime/reaper_backend_init.py
+++ src/renardo/runtime/reaper_backend_init.py
@@ -1,7 +1,14 @@
-from renardo.reaper_backend.ReaperIntegration import init_reapy_project, ReaperInstrumentFactory, ReaTask
+from renardo.reaper_backend.ReaperIntegration import init_reapy_project
+from renardo.reaper_backend.reaper_music_resource import ReaperInstrument
 
 old_style_presets = {}
 
 reaproject = init_reapy_project()
-reainstru_factory = ReaperInstrumentFactory(old_style_presets, reaproject)
+
+ReaperInstrument.initialize_factory(
+    presets=old_style_presets,
+    project=reaproject
+)
+
+#reainstru_factory = ReaperInstrumentFactory(old_style_presets, reaproject)
 #reaper_instruments = reainstru_factory.create_all_facades_from_reaproject_tracks()
