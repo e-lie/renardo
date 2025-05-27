2d861a856ccf22d4b229aeb002397de1d2c6c8d8
Elie Gavoty <eliegavoty@free.fr>
2025-05-27 10:17:54 +0200

    improve pit manipulation 17  tutorial

--- src/renardo/tutorial/en/17_point_in_time_manipulation.py
+++ src/renardo/tutorial/en/17_point_in_time_manipulation.py
@@ -1,9 +1,23 @@
-po1 = PointInTime()
+### Start in 16 beats
+
+pit1 = PointInTime()
 
 def haha():
     b1 >> blip()
     p2 >> play("ziuhf")
 
-Clock.schedule(haha, po1)
+Clock.schedule(haha, pit1)
 
-po1.beat=Clock.now()+16
+pit1.beat=Clock.now()+16
+
+
+### Start 16 beats before RDV
+
+def fadeinbloup():
+    b1 >> blip().fadein(16)
+
+pit2 = PointInTime() - 16
+
+Clock.schedule(fadeinbloup, pit2)
+
+pit2.beat=Clock.mod(32)
