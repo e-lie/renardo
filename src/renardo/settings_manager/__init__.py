5677736a27708c11566a127249fd500d172a461d
Elie Gavoty <eliegavoty@free.fr>
2025-05-27 19:58:06 +0200

    feat: add webclient setting in setting manager

--- src/renardo/settings_manager/__init__.py
+++ src/renardo/settings_manager/__init__.py
@@ -7,5 +7,6 @@ from .foxdot_editor_settings import Colours, conf
 from .sc_backend_settings import settings
 from .samples_pack_settings import settings
 from .webserver_settings import settings
+from .webclient_settings import settings
 from .reaper_backend_settings import settings
 
