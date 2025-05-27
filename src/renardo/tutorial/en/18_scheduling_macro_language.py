2cd27fe8c722801dc04a1b065f6169c3371c9216
Elie Gavoty <eliegavoty@free.fr>
2025-05-27 11:29:07 +0200

    chore: macro language for timing blocks

--- src/renardo/tutorial/en/18_scheduling_macro_language.py
+++ src/renardo/tutorial/en/18_scheduling_macro_language.py
@@ -2,12 +2,33 @@
 pit2 = PointInTime()
 
 # {Clock.now()+8}
-p1 >> pluck
+p1 >> pluck()
 
 # {Clock.mod(16)}
 b1 >> blip()
 
 # {pit2 - 16}
-b1.fadein(dur=16)
+b1.fadeout(dur=16)
 
-pit2.beat=Clock.now()
+pit2.beat=Clock.now() + 24
+
+
+
+
+
+########################
+
+pit5 = PointInTime()
+
+#{pit5} # this works
+p1 >> pluck()
+
+pit5.beat=Clock.now()+8
+
+
+pit6 = PointInTime()
+
+#{pit6 + 8} # this does not work
+p1 >> pluck()
+
+pit6.beat=Clock.now()+8
