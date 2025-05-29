292ef4cb740f6952986ab31b57d31fe003702a99
Elie Gavoty <eliegavoty@free.fr>
2025-05-30 00:30:49 +0200

    refactor: refactoring tutorials

---
+++ src/renardo/tutorial/en/19_state_rings.py
@@ -0,0 +1,18 @@
+
+### Rings as kinda meta Patterns
+
+# Rings are cycles of values that switch to next value each time the expression is evaluated
+
+b1 >> blip(R[2,3,P[2,3], var([2,1,4])]) # Will stay on note/degree 2, then when reevaluated stays on 3, then on Pattern [2,3] etc
+
+# Rings can contain any objects or values
+
+## Rings are meant to be used with recurrent points in time
+
+rpit1 = rpit()
+
+#{rpit1}
+b1 >> blip().stop(1.5)
+
+
+
