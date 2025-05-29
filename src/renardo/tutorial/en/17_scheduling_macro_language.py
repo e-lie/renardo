463482f3bf936bd0488e511236eaf441a31e1d12
Elie Gavoty <eliegavoty@free.fr>
2025-05-29 20:57:17 +0200

    iteration on tutorial

---
+++ src/renardo/tutorial/en/17_scheduling_macro_language.py
@@ -0,0 +1,63 @@
+### New scheduling feature PointInTime
+
+### Start in 16 beats
+
+pit1 = PointInTime()
+
+def whatever():
+    b1 >> blip([0,2,4,5], lpf=800)
+    p2 >> play("V..V*V.")
+
+Clock.schedule(whatever, pit1)
+
+pit1.beat=Clock.now()+16
+
+
+### Start 16 beats before RDV
+
+def fadeinbloup():
+    b1 >> blip().fadein(16)
+
+pit2 = pit() - 16 # pit is an alias for PointInTime
+
+Clock.schedule(fadeinbloup, pit2)
+
+pit2.beat=Clock.mod(32)
+
+
+
+# Tutorial : Using the new macro langage
+
+
+## Rather than using Clock.shecule on a function which is hard to livecode
+# and introduce a nee for indentation, renardo proposes a need macro langage compiled
+# at evaluation time
+
+# The basic syntax is #{ time expression }
+
+# For example the following...
+
+# {Clock.now()+8}
+p1 >> pluck()
+
+# ...is equivalent to :
+
+def pluck_func():
+    p1 >> pluck()
+Clock.schedule(pluck_func, Clock.now()+8)
+
+## Other examples
+
+# {Clock.mod(16)}
+b1 >> blip()
+
+## And with point in time class wich help manipulate future event more abstractly :
+
+pit2 = PointInTime()
+b1 >> blip(amplify=0)
+
+# {pit2} # schedules the the fadein at an undefined moment later
+b1.fadein(dur=16)
+
+# Set the undefined moment triggers the fadein at the right time
+pit2.beat=Clock.now() + 24
