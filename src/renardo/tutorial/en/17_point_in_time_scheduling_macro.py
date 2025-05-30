ebaa3a423676d85f40c3bfd344556996475f6aa4
Elie Gavoty <eliegavoty@free.fr>
2025-05-30 15:29:05 +0200

    improving tutorial

--- src/renardo/tutorial/en/17_point_in_time_scheduling_macro.py
+++ src/renardo/tutorial/en/17_point_in_time_scheduling_macro.py
@@ -1,30 +1,48 @@
 ### New scheduling feature PointInTime
 
-### Start in 16 beats
+### Start 16 beats later when defining Point in time value
 
-pit1 = PointInTime()
+b1 >> blip()
 
-def whatever():
-    b1 >> blip([0,2,4,5], lpf=800)
-    p2 >> play("V..V*V.")
+pit1 = PointInTime()  # or pit() to be shorter
 
-Clock.schedule(whatever, pit1)
 
-pit1.beat=Clock.now()+16 # the music declared in function whatever will start in 16 beats when you evaluate this line
+def footworking():
+    Clock.bpm = 160
+    b1 >> blip([0, 1, 5, [2, [_, 1]]], dur=var([.5, .25], 8), sus=1, amp=1.5 * P[.4, .7, 1, .7], lpf=800, oct=[4, 5, 6])
+    k2 >> play("v.(.x)(v*)*v.", dur=.25)
+    d2 >> play("{cccc.}", dur=var([2, 2 / 3], [12, 4]), rate=(1.2, 2.4), lpf=800)
 
-### You can also use substraction to start stuff before the point in time
+
+Clock.schedule(footworking, pit1)
+
+pit1.beat = Clock.now() + 16  # the music declared in function footworking will start in 16 beats when you evaluate this line
+
+### You can also use substraction (or other point in time arithmetic) to start stuff before the point in time
 # This way you can rendez vous at some point in time
 
-def fadeinbloup():
-    b1 >> blip().fadein(16)
+def somebreak_rise():
+    b1.fadein(32)  # rising melody for 32 beats before break
 
-pit2 = pit() - 16 # pit is an alias for PointInTime
 
-Clock.schedule(fadeinbloup, pit2)
+def somebreak():
+    d2.amplify = 0  # break (cutting the drum)
+    k2.amplify = 0
 
-pit2.beat=Clock.mod(32)
 
-# Using the new macro langage
+pit2 = pit()  # pit is an alias for PointInTime
+
+Clock.schedule(somebreak_rise, pit2 - 32)
+Clock.schedule(somebreak, pit2)
+
+Clock.bpm = 160
+b1 >> blip([0, 1, 5, [2, [_, 1]]], dur=var([.5, .25], 8), sus=1, amp=1.5 * P[.4, .7, 1, .7], lpf=800, oct=[4, 5, 6],
+           amplify=0)
+k2 >> play("v.(.x)(v*)*v.", dur=.25)
+d2 >> play("{cccc.}", dur=var([2, 2 / 3], [12, 4]), rate=(1.2, 2.4), lpf=800)
+pit2.beat = now() + 64
+
+# Doing the same using the new macro langage
 
 ## Rather than using Clock.shecule with a function which is hard and long to livecode
 # and introduce a need for python indentation, renardo introduces a new macro langage
@@ -34,27 +52,42 @@ pit2.beat=Clock.mod(32)
 
 # For example the following...
 
-# {Clock.now()+8}
-p1 >> pluck()
+# {Clock.now()+8} # starting in 8 beats
+Clock.bpm = 160
+b1 >> blip([0, 1, 5, [2, [_, 1]]], dur=var([.5, .25], 8), sus=1, amp=1.5 * P[.4, .7, 1, .7], lpf=800, oct=[4, 5, 6],
+           amplify=0)
+k2 >> play("v.(.x)(v*)*v.", dur=.25)
+d2 >> play("{cccc.}", dur=var([2, 2 / 3], [12, 4]), rate=(1.2, 2.4), lpf=800)
+
 
 # ...is equivalent to :
 
-def pluck_func():
-    p1 >> pluck()
-Clock.schedule(pluck_func, Clock.now()+8)
+def footworking_func():
+    Clock.bpm = 160
+    b1 >> blip([0, 1, 5, [2, [_, 1]]], dur=var([.5, .25], 8), sus=1, amp=1.5 * P[.4, .7, 1, .7], lpf=800, oct=[4, 5, 6],
+               amplify=0)
+    k2 >> play("v.(.x)(v*)*v.", dur=.25)
+    d2 >> play("{cccc.}", dur=var([2, 2 / 3], [12, 4]), rate=(1.2, 2.4), lpf=800)
 
-## Other examples
 
-# {Clock.mod(16)}
-b1 >> blip()
+Clock.schedule(footworking_func, Clock.now() + 8)
 
-## And with point in time class wich help manipulate future event more abstractly :
+## Preceding rendez vous example with macro syntax
 
-pit2 = PointInTime()
-b1 >> blip(amplify=0)
+pit3 = pit()
 
-# {pit2} # schedules the the fadein at an undefined moment later
-b1.fadein(dur=16)
+Clock.bpm = 160
+b1 >> blip([0, 1, 5, [2, [_, 1]]], dur=var([.5, .25], 8), sus=1, amp=1.5 * P[.4, .7, 1, .7], lpf=800, oct=[4, 5, 6],
+           amplify=0)
+k2 >> play("v.(.x)(v*)*v.", dur=.25)
+d2 >> play("{cccc.}", dur=var([2, 2 / 3], [12, 4]), rate=(1.2, 2.4), lpf=800)
 
-# Set the undefined moment triggers the fadein at the right time
-pit2.beat=Clock.now() + 24
+# {pit3-32}
+b1.fadein(32)  # rising melody for 32 beats before break
+# {pit3}
+d2.amplify = 0  # break (cutting the drum)
+k2.amplify = 0
+# {pit3+32}  # Bonus stopping 32 beat after the break
+Clock.clear()
+
+pit3.beat = now() + 64
