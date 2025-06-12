9a1e8a1955eb3fdf926bdca048f90227977ea6cc
Elie Gavoty <eliegavoty@free.fr>
2025-06-12 18:20:52 +0200

    refactor: move appState store to separate file

--- webclient/src/lib/websocket.js
+++ webclient/src/lib/websocket.js
@@ -1,33 +1,4 @@
-import { writable } from 'svelte/store';
-
-// Create stores for application state
-export const appState = writable({
-  counter: 0,
-  welcomeText: 'Loading...',
-  connected: false,
-  error: null,
-  // Add Renardo initialization state
-  renardoInit: {
-    superColliderClasses: false,
-    sclangCode: false,
-    samples: false,
-    instruments: false,
-    reaperPack: false,
-    startupFiles: false
-  },
-  // Add runtime status
-  runtimeStatus: {
-    scBackendRunning: false,
-    scBackendStartupCode: 'Renardo.start; Renardo.midi;',
-    renardoRuntimeRunning: false
-  },
-  // Add log messages array
-  logMessages: [],
-  // Add console output array
-  consoleOutput: [],
-  // Last received message for event detection
-  _lastMessage: null
-});
+import { appState } from './appState.js';
 
 // WebSocket connection
 let socket;
