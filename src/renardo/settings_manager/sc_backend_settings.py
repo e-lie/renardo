7c2289486b3ab5142ee45dcb8ea66df9e5ff24a2
Elie Gavoty <eliegavoty@free.fr>
2025-06-13 18:42:16 +0200

    customize num input and output + make renardo class auto update with params

--- src/renardo/settings_manager/sc_backend_settings.py
+++ src/renardo/settings_manager/sc_backend_settings.py
@@ -35,6 +35,8 @@ settings.set_defaults_from_dict({
         "SCCODE_LIBRARY_DIR_NAME": "sccode_library",
         "SPECIAL_SCCODE_DIR_NAME": "special_sccode",
         "DEFAULT_SCCODE_PACK_NAME": "0_foxdot_core",
+        "NUM_OUTPUT_BUS_CHANNELS": 8,
+        "NUM_INPUT_BUS_CHANNELS": 8,
     }
 },
 internal=True
