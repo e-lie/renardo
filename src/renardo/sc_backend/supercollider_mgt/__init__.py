00b9236b73867a7c1fda150ad03c9f871b21df57
Elie Gavoty <eliegavoty@free.fr>
2025-06-13 18:42:16 +0200

    customize num input and output + make renardo class auto update with params

--- src/renardo/sc_backend/supercollider_mgt/__init__.py
+++ src/renardo/sc_backend/supercollider_mgt/__init__.py
@@ -8,8 +8,16 @@ and file operations related to SuperCollider.
 from .sc_classes_files import (
     write_sc_renardo_files_in_user_config,
     is_renardo_sc_classes_initialized,
+    should_update_renardo_sc_classes,
     SC_USER_CONFIG_DIR,
     SC_USER_EXTENSIONS_DIR
 )
 
 from .sclang_instances_mgt import SupercolliderInstance
+
+def ensure_sc_classes_are_current():
+    """Ensure SuperCollider classes are up to date, auto-updating if necessary"""
+    if should_update_renardo_sc_classes():
+        print("SuperCollider classes are outdated, updating automatically...")
+        write_sc_renardo_files_in_user_config()
+        print("SuperCollider classes updated successfully.")
