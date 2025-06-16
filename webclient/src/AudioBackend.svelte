6d1ab57b4e0cd3834d5e2b5fac619d95052d25e4
Elie Gavoty <eliegavoty@free.fr>
2025-06-16 13:30:37 +0200

    Don't display audio device selection when backend is linux

--- webclient/src/AudioBackend.svelte
+++ webclient/src/AudioBackend.svelte
@@ -413,33 +413,35 @@
       <div class="lg:col-span-2">
         <div class="card bg-base-100 shadow-xl h-full">
           <div class="card-body">
-            <div class="flex items-center gap-3 mb-4">
-              <select 
-                class="select select-bordered flex-1" 
-                bind:value={selectedAudioOutputDevice}
-                on:change={saveAudioOutputDevice}
-                disabled={!$appState.connected}
-              >
-                <option value={-1}>System Default Audio Device</option>
-                {#each Object.entries(audioDevices.output) as [index, deviceName]}
-                  <option value={parseInt(index)}>{index}: {deviceName}</option>
-                {/each}
-              </select>
-              <button
-                class="btn btn-circle btn-ghost"
-                on:click={loadAudioDevices}
-                disabled={isLoadingAudioDevices || !$appState.connected}
-                title="Scan Audio Devices"
-              >
-                {#if isLoadingAudioDevices}
-                  <span class="loading loading-spinner loading-sm"></span>
-                {:else}
-                  <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                    <path fill-rule="evenodd" d="M4 2a1 1 0 011 1v2.101a7.002 7.002 0 0111.601 2.566 1 1 0 11-1.885.666A5.002 5.002 0 005.999 7H9a1 1 0 010 2H4a1 1 0 01-1-1V3a1 1 0 011-1zm.008 9.057a1 1 0 011.276.61A5.002 5.002 0 0014.001 13H11a1 1 0 110-2h5a1 1 0 011 1v5a1 1 0 11-2 0v-2.101a7.002 7.002 0 01-11.601-2.566 1 1 0 01.61-1.276z" clip-rule="evenodd" />
-                  </svg>
-                {/if}
-              </button>
-            </div>
+            {#if $appState.backendOS !== 'linux'}
+              <div class="flex items-center gap-3 mb-4">
+                <select 
+                  class="select select-bordered flex-1" 
+                  bind:value={selectedAudioOutputDevice}
+                  on:change={saveAudioOutputDevice}
+                  disabled={!$appState.connected}
+                >
+                  <option value={-1}>System Default Audio Device</option>
+                  {#each Object.entries(audioDevices.output) as [index, deviceName]}
+                    <option value={parseInt(index)}>{index}: {deviceName}</option>
+                  {/each}
+                </select>
+                <button
+                  class="btn btn-circle btn-ghost"
+                  on:click={loadAudioDevices}
+                  disabled={isLoadingAudioDevices || !$appState.connected}
+                  title="Scan Audio Devices"
+                >
+                  {#if isLoadingAudioDevices}
+                    <span class="loading loading-spinner loading-sm"></span>
+                  {:else}
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                      <path fill-rule="evenodd" d="M4 2a1 1 0 011 1v2.101a7.002 7.002 0 0111.601 2.566 1 1 0 11-1.885.666A5.002 5.002 0 005.999 7H9a1 1 0 010 2H4a1 1 0 01-1-1V3a1 1 0 011-1zm.008 9.057a1 1 0 011.276.61A5.002 5.002 0 0014.001 13H11a1 1 0 110-2h5a1 1 0 011 1v5a1 1 0 11-2 0v-2.101a7.002 7.002 0 01-11.601-2.566 1 1 0 01.61-1.276z" clip-rule="evenodd" />
+                    </svg>
+                  {/if}
+                </button>
+              </div>
+            {/if}
             
             <!-- Action buttons -->
             <div class="flex gap-2">
@@ -557,8 +559,12 @@
             Launch SuperCollider IDE
           </button></li>
             <li>Run the following code (Ctrl+Enter): </li>
-            <li><code class="bg-base-300 px-2 py-1 rounded">Renardo.listAudioDevices()</code></li>
-            <li><code class="bg-base-300 px-2 py-1 rounded">Renardo.start(audio_device_index: -1)</code>  // (-1 means default device or you can choose a device from the list by index)</li>
+            {#if $appState.backendOS !== 'linux'}
+              <li><code class="bg-base-300 px-2 py-1 rounded">Renardo.listAudioDevices()</code></li>
+              <li><code class="bg-base-300 px-2 py-1 rounded">Renardo.start(audio_device_index: -1)</code>  // (-1 means default device or you can choose a device from the list by index)</li>
+            {:else}
+              <li><code class="bg-base-300 px-2 py-1 rounded">Renardo.start()</code></li>
+            {/if}
             <li><code class="bg-base-300 px-2 py-1 rounded">Renardo.midi()</code></li>
           </ol>
           
