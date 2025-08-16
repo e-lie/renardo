b8a5037e8a6afc63ac6cdf0ebda741de8e489e85
Elie Gavoty <eliegavoty@free.fr>
2025-08-16 18:00:42 +0200

    refactor: remove old osc mechanisms

--- src/renardo/reaper_backend/reaside/tools/__init__.py
+++ src/renardo/reaper_backend/reaside/tools/__init__.py
@@ -6,13 +6,6 @@ from .reaper_client import ReaperClient
 # Import the HTTP-only client for backwards compatibility and initialization
 from .reaper_http_client import ReaperClient as ReaperHTTPClient, ReaperHTTPError, ReaperAPIError, ReaperNotFoundError
 
-# Import OSC client (with fallback if python-osc not available)
-try:
-    from .reaper_osc_client import ReaperOSCClient, ReaperOSCError
-except ImportError:
-    # python-osc not available
-    ReaperOSCClient = None
-    ReaperOSCError = None
 
 # Import from the reaper_program module
 from .reaper_program import start_reaper, stop_reaper, is_reaper_running
