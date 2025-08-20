569f357424c252e158d73ea2f322121bdf5083c6
Elie Gavoty <eliegavoty@free.fr>
2025-08-20 17:24:59 +0200

    chore: removing legacy reaper integration + instrument_dict class parameters

--- src/renardo/reaper_backend/__init__.py
+++ src/renardo/reaper_backend/__init__.py
@@ -1,6 +1,5 @@
 from renardo.reaper_backend.reaper_instrument import ReaperInstrument
 from renardo.reaper_backend.reaper_effect import ReaperEffect
-from renardo.reaper_backend.ReaperIntegration import init_reapy_project
 from renardo.reaper_backend.reaper_simple_lib import ensure_16_midi_tracks
 
 from renardo.reaper_backend.reaside import (
