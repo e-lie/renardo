5e62f9c5e9828f45b0fed9fb2da93303dccc00b7
Elie Gavoty <eliegavoty@free.fr>
2025-08-21 16:15:12 +0200

    chore: factorise reaper resources code

--- src/renardo/reaper_backend/__init__.py
+++ src/renardo/reaper_backend/__init__.py
@@ -1,3 +1,4 @@
+from renardo.reaper_backend.reaper_resource import ReaperResource
 from renardo.reaper_backend.reaper_instrument import ReaperInstrument
 from renardo.reaper_backend.reaper_effect import ReaperEffect
 from renardo.reaper_backend.reaper_simple_lib import ensure_16_midi_tracks
