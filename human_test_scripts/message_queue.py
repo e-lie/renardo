c42d0f7f22c414e8161e4e7b13eff494f12bb233
Elie Gavoty <eliegavoty@free.fr>
2025-08-23 16:07:03 +0200

    fix: fix arguments parsing

--- human_test_scripts/message_queue.py
+++ human_test_scripts/message_queue.py
@@ -20,12 +20,14 @@ messages = [
 
 
 for j in range(100):
-    for i, msg in enumerate(messages, 1):
-        # Send message to ExtState queue
-        client.set_ext_state("reaside_queue", "message", json.dumps(msg))
-        print(msg)
-        # Small delay to let Reaper process
-        time.sleep(0.01)
+    mmsg = {"action": "test_message", "args": [j]}
+    client.set_ext_state("reaside_queue", "message", json.dumps(mmsg))
+    # for i, msg in enumerate(messages, 1):
+    #     # Send message to ExtState queue
+    #     client.set_ext_state("reaside_queue", "message", json.dumps(msg))
+    #     print(msg)
+    #     # Small delay to let Reaper process
+    #     time.sleep(0.001)
 
 # Send an unknown action to test error handling
 client.set_ext_state("reaside_queue", "message", json.dumps({"action": "unknown_action", "args": ["test"]}))
