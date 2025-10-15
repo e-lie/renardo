74d99cc2d7f5fd2e1a0a560cd7501e2f1ecae421
Elie Gavoty <eliegavoty@free.fr>
2025-10-15 14:04:18 -0400

    flok iframe integration WIP

--- webclient_fresh/src/lib/stores.ts
+++ webclient_fresh/src/lib/stores.ts
@@ -3,3 +3,4 @@ import { writable } from 'svelte/store'
 // Simple router store
 export const currentPage = writable('posts')
 export const selectedPost = writable(null)
+export const currentSession = writable('renardo-session')
