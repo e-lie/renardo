a4f672f5e6165098f9148f038c2f15ca47dc9bd9
Elie Gavoty <eliegavoty@free.fr>
2025-10-15 11:07:33 -0400

    vscode run config + clean old md + remove reaper_simple_lib and refs

--- src/renardo/reaper_backend/__init__.py
+++ src/renardo/reaper_backend/__init__.py
@@ -1,7 +1,6 @@
 from renardo.reaper_backend.reaper_resource import ReaperResource
 from renardo.reaper_backend.reaper_instrument import ReaperInstrument
 from renardo.reaper_backend.reaper_effect import ReaperEffect
-from renardo.reaper_backend.reaper_simple_lib import ensure_16_midi_tracks
 
 from renardo.reaper_backend.reaside import (
     start_reaper, stop_reaper,
