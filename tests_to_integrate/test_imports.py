03c63ba8c5138f22883e3b039c92f9ba846ea48d
Elie Gavoty <eliegavoty@free.fr>
2025-11-08 19:41:34 +0100

    chore: new README

--- tests_to_integrate/test_imports.py
+++
@@ -1,9 +0,0 @@
-print("Starting import test...")
-
-try:
-    from renardo.runtime import *
-    print("Successfully imported renardo.runtime")
-except Exception as e:
-    print(f"Error importing renardo.runtime: {e}")
-    import traceback
-    traceback.print_exc()
