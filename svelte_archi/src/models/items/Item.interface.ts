0d7f4936f6916545583f5eee2d9e60a23d27af00
Elie Gavoty <eliegavoty@free.fr>
2025-11-11 13:50:42 -0500

    chore: update gitignore .vite + hide svelte archi example dir

--- svelte_archi/src/models/items/Item.interface.ts
+++
@@ -1,7 +0,0 @@
-// file: src/models/items/Item.interface.ts
-
-export interface ItemInterface {
-  id: number
-  name: string
-  selected: boolean
-}
--- svelte_archi/src/views/Home.view.svelte
+++
@@ -1,6 +0,0 @@
-<script lang="ts">
-</script>
-
-<div>
-  <h1>Home View</h1>
-</div>
