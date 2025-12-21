dd74aefd4d3bfe6606dc7c95ab3b35effd3e0399
Elie Gavoty <eliegavoty@free.fr>
2025-12-21 20:17:06 +0100

    remove refresh tutorial button

--- webclient_fresh/src/components/tutorial/TutorialBrowser.component.svelte
+++ webclient_fresh/src/components/tutorial/TutorialBrowser.component.svelte
@@ -68,14 +68,5 @@
         <ElText tag="p" text={`No tutorials available for ${$selectedLanguage}.`} addCss="text-gray-500" />
       {/if}
     {/if}
-
-    <div class="mt-6 pt-4 border-t">
-      <ElButton variant="ghost" onclick={() => loadTutorialFiles()}>
-        <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15" />
-        </svg>
-        Refresh
-      </ElButton>
-    </div>
   </div>
 </ElCard>
