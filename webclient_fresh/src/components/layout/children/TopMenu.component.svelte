6ed6680cdd1fbc98b7779a412605b1219d9de88b
Elie Gavoty <eliegavoty@free.fr>
2025-12-26 18:50:36 +0100

    translate select code folder button

--- webclient_fresh/src/components/layout/children/TopMenu.component.svelte
+++ webclient_fresh/src/components/layout/children/TopMenu.component.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
   import { useProjectStore } from '../../../store/project'
+  import { useI18nStore } from '../../../store/i18n/I18n.store'
   import FileExplorerModal from '../../shared/FileExplorerModal.component.svelte'
 
   const { actions: projectActions, getters: projectGetters } = useProjectStore()
   const { currentProject } = projectGetters
+
+  const { getters: i18nGetters } = useI18nStore()
+  const { translate } = i18nGetters
 
   let showFileExplorer = $state(false)
 
@@ -23,7 +27,7 @@
       <button
         class="btn btn-sm variant-ghost"
         onclick={() => showFileExplorer = true}
-        title="Open Project"
+        title={$translate('selectCodeFolder')}
       >
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-4 h-4">
           <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12.75V12A2.25 2.25 0 014.5 9.75h15A2.25 2.25 0 0121.75 12v.75m-8.69-6.44l-2.12-2.12a1.5 1.5 0 00-1.061-.44H4.5A2.25 2.25 0 002.25 6v12a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9a2.25 2.25 0 00-2.25-2.25h-5.379a1.5 1.5 0 01-1.06-.44z" />
@@ -31,7 +35,7 @@
         {#if $currentProject}
           <span class="text-xs">{$currentProject.root_path.split('/').pop()}</span>
         {:else}
-          <span class="text-xs">Project</span>
+          <span class="text-xs">{$translate('selectCodeFolder')}</span>
         {/if}
       </button>
     </div>
