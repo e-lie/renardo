fc2f7e5e7ec09947c67dd6db98ea79bb7afb48e0
Elie Gavoty <eliegavoty@free.fr>
2026-01-02 17:22:33 +0100

    theme transparent !!

--- webclient_fresh/src/main.ts
+++ webclient_fresh/src/main.ts
@@ -1,6 +1,10 @@
 import './app.css'
 import { mount } from 'svelte'
 import App from './App.svelte'
+import { initBackgroundCanvas } from './background-canvas'
+
+// Initialize background canvas
+initBackgroundCanvas()
 
 const app = mount(App, {
   target: document.getElementById('app')!,
