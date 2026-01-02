b1f9b441b348f5e9b0fd81ebc5a4ad3927fd563b
Elie Gavoty <eliegavoty@free.fr>
2026-01-02 16:49:44 +0100

    completing topmenu + add alt-enter and shift-enter execution modes

--- webclient_fresh/src/store/root/models/RootStore.interface.ts
+++ webclient_fresh/src/store/root/models/RootStore.interface.ts
@@ -2,10 +2,12 @@ import type { TutorialStoreInterface } from '../../tutorial/models'
 import type { MusicExampleStoreInterface } from '../../music-example/models'
 import type { UserDirectoryStoreInterface } from '../../user-directory/models'
 import type { WebSocketBackendStoreInterface } from '../../websocket/models'
+import type { EditorStoreInterface } from '../../editor/models'
 
 export interface RootStoreInterface {
   tutorialStore: TutorialStoreInterface
   musicExampleStore: MusicExampleStoreInterface
   userDirectoryStore: UserDirectoryStoreInterface
   webSocketBackendStore: WebSocketBackendStoreInterface
+  editorStore: EditorStoreInterface
 }
